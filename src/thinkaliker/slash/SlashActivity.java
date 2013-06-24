6e2dbe0487c7ded8208d68b8e7d6b59253fd90c3
thinkaliker <me@thinkaliker.tk>
2013-06-23 23:34:26 -0700

    Slash activity - modified for title screen

--- src/thinkaliker/slash/SlashActivity.java
+++ src/thinkaliker/slash/SlashActivity.java
@@ -2,6 +2,8 @@ package thinkaliker.slash;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SlashActivity extends Activity {
     /**
@@ -11,6 +13,9 @@ public class SlashActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TitleView tView = new TitleView(this);
+        tView.setKeepScreenOn(true);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(tView);
     }
 }
