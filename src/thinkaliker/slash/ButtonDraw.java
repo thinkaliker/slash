7d0cfce1e97aa2d88a2a0ae89ba7fe094d4653a0
thinkaliker <me@thinkaliker.tk>
2013-06-23 23:35:11 -0700

    ButtonDraw class initial

---
+++ src/thinkaliker/slash/ButtonDraw.java
@@ -0,0 +1,22 @@
+package thinkaliker.slash;
+
+import android.graphics.*;
+
+/**
+ * Created by: Adam
+ * Date: 6/23/13 - 8:06 PM
+ */
+public class ButtonDraw {
+
+    private Paint buttonPaint = new Paint();
+
+    public ButtonDraw(int startX, int startY, int endX, int endY, int color1R, int color1G, int color1B, int color2R, int color2G, int color2B){
+      int color1 = Color.rgb(color1R, color1G, color1B);
+      int color2 = Color.rgb(color2R, color2G, color2B);
+
+      buttonPaint.setShader(new LinearGradient(startX, startY, endX, endY, color1, color2, Shader.TileMode.MIRROR ));
+    }
+    public Paint ButtonPaint(){
+      return buttonPaint;
+    }
+}
